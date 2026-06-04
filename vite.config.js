aa57cbd76c9726673b01c3f4ed99d5f134a7ba50
Rojan Shrestha <shrestharojan79@gmail.com>
2026-06-04 22:28:00 +0545

    fix : yaml file added

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5176",
-      "/media": "http://localhost:5176"
-    }
-  }
+  plugins: [react()]
 });
